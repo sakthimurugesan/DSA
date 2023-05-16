4d2bd50ca6c9f190206bfd2e29ffac8d4d653f93
SAKTHI M <msakthi150@gmail.com>
2023-05-16 14:08:56 +0530

    commit

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 struct Node
@@ -8,59 +8,62 @@ struct Node
     struct Node *right;
 };
 
-struct Node* create()
+struct Node *create()
 {
     int x;
     struct Node *newnode = new Node();
-    cin>>x;
-    if(x==-1)
+    cin >> x;
+    if (x == -1)
     {
         return 0;
     }
-    newnode->data=x;
-    cout<<"Left child Data for "<<x<<" ";
+    newnode->data = x;
+    cout << "Left child Data for " << x << " ";
     newnode->left = create();
-    cout<<"\nRight child Data for "<<x<<" ";
+    cout << "\nRight child Data for " << x << " ";
     newnode->right = create();
     return newnode;
 }
 void postOrder(struct Node *root)
 {
-    if(root==0){
+    if (root == 0)
+    {
         return;
     }
     postOrder(root->left);
     postOrder(root->right);
-    cout<<root->data<<" ";
+    cout << root->data << " ";
 }
 void preOrder(struct Node *root)
 {
-     if(root==0){
+    if (root == 0)
+    {
         return;
     }
-    cout<<root->data<<" ";
+    cout << root->data << " ";
     preOrder(root->left);
     preOrder(root->right);
 }
 void inOrder(struct Node *root)
 {
-     if(root==0){
+    if (root == 0)
+    {
         return;
     }
     preOrder(root->left);
-    cout<<root->data<<" ";
+    cout << root->data << " ";
     preOrder(root->right);
 }
-
-
 
 int main()
 {
     struct Node *root;
     root = NULL;
     root = create();
-    preOrder(root);cout<<endl;
-    postOrder(root);cout<<endl;
-    inOrder(root);cout<<endl;
-
+    preOrder(root);
+    cout << endl;
+    postOrder(root);
+    cout << endl;
+    inOrder(root);
+    cout << endl;
 }
