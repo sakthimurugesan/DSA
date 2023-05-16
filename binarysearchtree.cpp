35666d8b072c8f9a49fe8627969fe94465b11dde
SAKTHI M <msakthi150@gmail.com>
2023-05-16 11:17:53 +0530

    commit

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -2,53 +2,64 @@
 
 using namespace std;
 
-struct TreeNode {
+struct TreeNode
+{
     int val;
-    TreeNode* left;
-    TreeNode* right;
+    TreeNode *left;
+    TreeNode *right;
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-TreeNode* insert(TreeNode* node, int val) {
-    if (node == NULL) {
+TreeNode *insert(TreeNode *node, int val)
+{
+    if (node == NULL)
+    {
         return new TreeNode(val);
     }
-    
-    if (val < node->val) {
+
+    if (val < node->val)
+    {
         node->left = insert(node->left, val);
-    } else {
+    }
+    else
+    {
         node->right = insert(node->right, val);
     }
-    
+
     return node;
 }
 
-void inorderDisplay(TreeNode* node) {
-    if (node == NULL) {
+void inorderDisplay(TreeNode *node)
+{
+    if (node == NULL)
+    {
         return;
     }
-    
+
     inorderDisplay(node->left);
     cout << node->val << " ";
     inorderDisplay(node->right);
 }
 
-int main() {
-    TreeNode* root = NULL;
+int main()
+{
+    TreeNode *root = NULL;
     int val;
-    
-    while (true) {
+
+    while (true)
+    {
         cout << "Enter a value to insert (or -1 to stop): ";
         cin >> val;
-        
-        if (val == -1) {
+
+        if (val == -1)
+        {
             break;
         }
-        
+
         root = insert(root, val);
     }
-    
+
     inorderDisplay(root);
-    
+
     return 0;
 }
--- reference.cpp
+++ reference.cpp
@@ -1,15 +1,32 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
+void addEdge(vector<int> adj[], int u, int v)
+{
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+void printGraph(vector<int> adj[], int V)
+{
+    for (int v = 0; v < V; ++v)
+    {
+        cout << v;
+        for (int i = 0; i < adj[v].size(); i++)
+            cout << " -> " << adj[v][i];
+        printf("\n");
+    }
+} // Driver code
 int main()
 {
-    int a = 10;
-    cout<<&a<<endl;
-    int *p;
-    p = &a;
-    cout<<p<<endl;
-    cout<<*p<<endl;
-    *p = 11;
-    cout<<a<<endl;
-    cout<<&a<<endl;
+    int V, a, b;
+    cin >> V;
+    int E, c;
+    cin >> E;
+    vector<int> adj[V];
+    for (int i = 0; i < E; i++)
+    {
+        cin >> a >> b;
+        addEdge(adj, a, b);
+    }
+    printGraph(adj, V);
     return 0;
 }
